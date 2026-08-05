d9a34d4e6ae32dfd0e51399ade4c68784ec7f9fc
SumanSingha05 <sumansingha8654@gmail.com>
2026-08-05 21:38:28 +0530

    Update backend handler to return JSON and switch server port to 3000

--- backend/main.go
+++ backend/main.go
@@ -3,18 +3,30 @@ package main
 import (
 	"fmt" // used for printing
 	"net/http" // Go's built-in web server library.
+	"log"
+	"encoding/json"
 )
 
+type Response struct {
+	Message string `json:"message"`
+}
+
 func home(w http.ResponseWriter, r *http.Request) { // handler, w = ResponseWriter, r = request
-	fmt.Println("Welcome to Anonymous Msg Backend")
+	w.Header().Set("Content-Type", "application/json")
+
+	response := Response{
+		Message: "API Is Running",
+	}
+
+	json.NewEncoder(w).Encode(response)
 }
 
 func main() {
 	http.HandleFunc("/", home)
 
-	fmt.Println("Server running on http://localhost:8080")
+	fmt.Println("Server running on http://localhost:3000")
 
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":3000", nil))
 
 
 }
