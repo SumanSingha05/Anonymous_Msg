120dd42ba12625cdf7cd80f7b2156734ab158be7
SumanSingha05 <sumansingha8654@gmail.com>
2026-08-05 21:28:26 +0530

    setup: first go setup initialization from the backend done.

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt" // used for printing
+	"net/http" // Go's built-in web server library.
+)
+
+func home(w http.ResponseWriter, r *http.Request) { // handler, w = ResponseWriter, r = request
+	fmt.Println("Welcome to Anonymous Msg Backend")
+}
 
 func main() {
-	fmt.Println("Welcome to Anonymous Msg Backend")
+	http.HandleFunc("/", home)
+
+	fmt.Println("Server running on http://localhost:8080")
+
+	http.ListenAndServe(":8080", nil)
+
+
 }
